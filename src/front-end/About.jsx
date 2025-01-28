61144d81dff97a61961906d3f7c559c313b448c5
Arjun kushwaha <arjun.kushwah@myphoneme.com>
2025-01-28 18:08:54 +0530

    testing jenkins auto-deploy-3

--- src/front-end/About.jsx
+++ src/front-end/About.jsx
@@ -17,9 +17,9 @@ function About() {
       >
         <Row className="justify-content-center">
           <Col md={10} className="text-center">
-            <h3 className="mb-3">Who We Are --?</h3>
+            <h3 className="mb-3">Who We Are ?</h3>
             <p>
-              Hello there , Founded by IITians, with the objective of making
+              Founded by IITians, with the objective of making
               technologies friendly to human beings. The company’s main
               objective is to reach masses and especially in the developing
               nations where internet and literacy are the major challenges.
